51ff3fc082da53794039719d346e609d36abc99f
Swayam Kumar Karn <rohankukarn75@gmail.com>
2024-08-14 03:05:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,26 +3,30 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import { admissionRoute } from './routes/admissionRoute.js';
 import { certificateRoute } from './routes/certificateRoute.js';
-import cors from 'cors'
-
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 connectDB();
 
-app.use(cors());
+// CORS configuration to allow all origins
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed methods
+  allowedHeaders: 'Content-Type,Authorization' // Specify allowed headers
+}));
+
 app.use(express.json());
 
-app.use('/admission' , admissionRoute)
-app.use('/certificate' , certificateRoute)
+app.use('/admission', admissionRoute);
+app.use('/certificate', certificateRoute);
 
-
-app.get('/', async(req, res)=>{
-   res.json({"Message ": "Running Successfully."});
-})
+app.get('/', async (req, res) => {
+  res.json({ "Message": "Running Successfully." });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server runing on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
