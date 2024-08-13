92381be414ec12946f59a27f3ab284e1520d7bfc
Swayam Kumar Karn <rohankukarn75@gmail.com>
2024-08-14 03:24:55 +0530

    Update admissionRoute.js

--- routes/admissionRoute.js
+++ routes/admissionRoute.js
@@ -6,6 +6,16 @@ const router = express.Router();
 router.post('/create' , createAdmission);
 router.get('/get', getAllAdmissions);
 
+router.delete('/all', async (req, res) => {
+  try {
+    // Delete all documents from the admission collection
+    await Admission.deleteMany({});
+    
+    res.status(200).json({ message: 'All admissions have been deleted successfully.' });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to delete admissions.', error });
+  }
+});
 
 export {router as admissionRoute}
 
